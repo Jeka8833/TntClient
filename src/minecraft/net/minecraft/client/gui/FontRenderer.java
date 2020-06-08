a77e1186e6cef9e88e574b9333444afdbc14340a
Jeka8833 <eadercach@gmail.com>
2020-06-08 18:11:27 +0300

    Fix optifine font bug

--- src/minecraft/net/minecraft/client/gui/FontRenderer.java
+++ src/minecraft/net/minecraft/client/gui/FontRenderer.java
@@ -36,7 +36,7 @@ public class FontRenderer implements IResourceManagerReloadListener {
     /**
      * Array of width of all the characters in default.png
      */
-    private float[] charWidth = new float[256];
+    private final float[] charWidth = new float[256];
 
     /**
      * the height in pixels of default text
@@ -47,13 +47,13 @@ public class FontRenderer implements IResourceManagerReloadListener {
     /**
      * Array of the start/end column (in upper/lower nibble) for every glyph in the /font directory.
      */
-    private byte[] glyphWidth = new byte[65536];
+    private final byte[] glyphWidth = new byte[65536];
 
     /**
      * Array of RGB triplets defining the 16 standard chat colors followed by 16 darker version of the same colors for
      * drop shadows.
      */
-    private int[] colorCode = new int[32];
+    private final int[] colorCode = new int[32];
     private ResourceLocation locationFontTexture;
 
     /**
@@ -110,13 +110,11 @@ public class FontRenderer implements IResourceManagerReloadListener {
      * Set if the "m" style (strikethrough) is active in currently rendering string
      */
     private boolean strikethroughStyle;
-    public GameSettings gameSettings;
     public ResourceLocation locationFontTextureBase;
     public boolean enabled = true;
     public float offsetBold = 1.0F;
 
     public FontRenderer(GameSettings gameSettingsIn, ResourceLocation location, TextureManager textureManagerIn, boolean unicode) {
-        this.gameSettings = gameSettingsIn;
         this.locationFontTextureBase = location;
         this.locationFontTexture = location;
         this.renderEngine = textureManagerIn;
@@ -598,15 +596,10 @@ public class FontRenderer implements IResourceManagerReloadListener {
             return this.charWidth[32];
         } else {
             int i = "\u00c0\u00c1\u00c2\u00c8\u00ca\u00cb\u00cd\u00d3\u00d4\u00d5\u00da\u00df\u00e3\u00f5\u011f\u0130\u0131\u0152\u0153\u015e\u015f\u0174\u0175\u017e\u0207\u0000\u0000\u0000\u0000\u0000\u0000\u0000 !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\u0000\u00c7\u00fc\u00e9\u00e2\u00e4\u00e0\u00e5\u00e7\u00ea\u00eb\u00e8\u00ef\u00ee\u00ec\u00c4\u00c5\u00c9\u00e6\u00c6\u00f4\u00f6\u00f2\u00fb\u00f9\u00ff\u00d6\u00dc\u00f8\u00a3\u00d8\u00d7\u0192\u00e1\u00ed\u00f3\u00fa\u00f1\u00d1\u00aa\u00ba\u00bf\u00ae\u00ac\u00bd\u00bc\u00a1\u00ab\u00bb\u2591\u2592\u2593\u2502\u2524\u2561\u2562\u2556\u2555\u2563\u2551\u2557\u255d\u255c\u255b\u2510\u2514\u2534\u252c\u251c\u2500\u253c\u255e\u255f\u255a\u2554\u2569\u2566\u2560\u2550\u256c\u2567\u2568\u2564\u2565\u2559\u2558\u2552\u2553\u256b\u256a\u2518\u250c\u2588\u2584\u258c\u2590\u2580\u03b1\u03b2\u0393\u03c0\u03a3\u03c3\u03bc\u03c4\u03a6\u0398\u03a9\u03b4\u221e\u2205\u2208\u2229\u2261\u00b1\u2265\u2264\u2320\u2321\u00f7\u2248\u00b0\u2219\u00b7\u221a\u207f\u00b2\u25a0\u0000".indexOf(p_getCharWidthFloat_1_);
-
             if (p_getCharWidthFloat_1_ > 0 && i != -1 && !this.unicodeFlag) {
                 return this.charWidth[i];
             } else if (this.glyphWidth[p_getCharWidthFloat_1_] != 0) {
-                int j = this.glyphWidth[p_getCharWidthFloat_1_] >>> 4;
-                int k = this.glyphWidth[p_getCharWidthFloat_1_] & 15;
-                j = j & 15;
-                ++k;
-                return (float) ((k - j) / 2 + 1);
+                return ((this.glyphWidth[p_getCharWidthFloat_1_] & 15) - ((this.glyphWidth[p_getCharWidthFloat_1_] >>> 4) & 15) + 3) / 2f;
             } else {
                 return 0.0F;
             }
